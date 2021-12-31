53ea1b00e264a0b629788aae0b68b9d92d2ecdea
p-nuts5963 <96852573+p-nuts5963@users.noreply.github.com>
2021-12-31 22:03:22 +0900

    creating header

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -48,15 +48,16 @@ export const Header: VFC = memo(() => {
           size="sm"
           variant="unstyled"
           display={{ base: "blick", md: "none" }}
+          onClick={onOpen}
         />
       </Flex>
-      {/* <Drawer placement="left" size="xs" onClose={onClose} isOpen> */}
+      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay>
           <DrawerContent>
-            <DrawerBody>
-              <Button>TOP</Button>
-              <Button>ユーザー一覧</Button>
-              <Button>設定</Button>
+            <DrawerBody p={0} bg="gray.100">
+              <Button w="100%">TOP</Button>
+              <Button w="100%">ユーザー一覧</Button>
+              <Button w="100%">設定</Button>
             </DrawerBody>
           </DrawerContent>
         </DrawerOverlay>
