d656fd93e5ebc7425b86dc7712b536e03c0126ad
p-nuts5963 <96852573+p-nuts5963@users.noreply.github.com>
2022-01-01 04:17:41 +0900

    try saving login user information in context

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -8,14 +8,16 @@ import {
 } from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { useSelectUser } from "../../hooks/useSelectUser";
-import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useLoginUser } from "../../hooks/useLoginUser";
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
   useEffect(() => getUsers(), [getUsers]);
   const onClickUser = useCallback(
     (id: number) => {
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,10 +4,12 @@ import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
 import { useMessage } from "./useMessage";
+import { useLoginUser } from "../hooks/useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
+  const { setLoginUser } = useLoginUser();
   const [loading, setLoading] = useState(false);
   const login = useCallback(
     (id: string) => {
@@ -16,6 +18,7 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
@@ -23,14 +26,15 @@ export const useAuth = () => {
               title: "ユーザーが見つかりません",
               status: "error"
             });
+            setLoading(false);
           }
         })
-        .catch(() =>
-          showMessage({ title: "ログインできません", status: "error" })
-        )
-        .finally(() => setLoading(false));
+        .catch(() => {
+          showMessage({ title: "ログインできません", status: "error" });
+          setLoading(false);
+        });
     },
-    [history, showMessage]
+    [history, showMessage, setLoginUser]
   );
   return { login, loading };
 };
